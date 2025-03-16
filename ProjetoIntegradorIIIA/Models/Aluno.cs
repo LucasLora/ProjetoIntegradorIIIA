4025560e6eb5d5c6312893b39d7915ed147a624a
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-03-16 16:01:41 -0300

    Create Aluno

---
+++ ProjetoIntegradorIIIA/Models/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjetoIntegradorIIIA.Models
+{
+    public class Aluno
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Aluno(int codigo, string nome, string cpf, string endereco, DateTime dataNascimento)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Cpf = cpf;
+            Endereco = endereco;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
