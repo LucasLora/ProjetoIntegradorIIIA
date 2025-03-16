9f61a1bd5304e4531c3aaef749ac86ccb0bb07e2
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-03-16 17:17:20 -0300

    Create ListaDeAluno

---
+++ ProjetoIntegradorIIIA/ListaDeAluno/ListaDeAluno.cs
@@ -0,0 +1,35 @@
+﻿using ProjetoIntegradorIIIA.CustomLinkedList;
+using ProjetoIntegradorIIIA.Models;
+
+namespace ProjetoIntegradorIIIA.ListaDeAluno
+{
+    public class ListaDeAluno
+    {
+        private CustomLinkedList<Aluno> lista;
+
+        public ListaDeAluno() => lista = new CustomLinkedList<Aluno>();
+
+        public void IncluirNoInicio(Aluno aluno) => lista.AddFirst(aluno);
+
+        public void IncluirNoFim(Aluno aluno) => lista.AddLast(aluno);
+
+        public void Ordenar() => lista.Sort((a, b) => string.Compare(a.Nome, b.Nome, StringComparison.OrdinalIgnoreCase));
+
+        public void RemoverDoInicio() => lista.RemoveFirst();
+
+        public void RemoverDoFim() => lista.RemoveLast();
+
+        public int Tamanho() => lista.Count;
+
+        public Aluno? GetByCodigo(int codigo)
+        {
+            for (CustomLinkedListNode<Aluno> node = lista.First; node != null; node = node.Next)
+            {
+                if (node.Value.Codigo == codigo)
+                    return node.Value;
+            }
+
+            return null;
+        }
+    }
+}
