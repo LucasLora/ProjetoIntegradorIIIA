feeb486d7fac742431f810d63628be91016d72e1
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-03-16 16:02:01 -0300

    Create Turma

---
+++ ProjetoIntegradorIIIA/Enums/EtapaEnsinoEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoIntegradorIIIA.Enums
+{
+    public enum EtapaEnsinoEnum
+    {
+        Infantil = 0,
+        FundamentalAnosIniciais = 1,
+        FundamentalAnosFinais = 2,
+        Medio = 3,
+    }
+}
---
+++ ProjetoIntegradorIIIA/Models/Turma.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoIntegradorIIIA.Enums;
+
+namespace ProjetoIntegradorIIIA.Models
+{
+    public class Turma
+    {
+        public int Codigo { get; set; }
+        public EtapaEnsinoEnum EtapaEnsino { get; set; }
+        public byte Ano { get; set; }
+        public int LimiteVagas { get; set; }
+        public int QuantidadeMatriculados { get; set; }
+
+        public Turma(int codigo, EtapaEnsinoEnum etapaEnsino, byte ano, int limiteVagas)
+        {
+            Codigo = codigo;
+            EtapaEnsino = etapaEnsino;
+            Ano = ano;
+            LimiteVagas = limiteVagas;
+            QuantidadeMatriculados = 0;
+        }
+    }
+}
+
