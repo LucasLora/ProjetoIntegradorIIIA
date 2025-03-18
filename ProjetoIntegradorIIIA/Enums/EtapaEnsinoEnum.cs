39ba4b41af65749be369fda6e7985e73b3b77fd9
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-03-17 21:03:57 -0300

    Feature - Escola Implementation
    * Create Escola
    
    * Update Aluno and override ToString()
    
    * Add description to EtapaEnsinoEnum and create EnumHelper
    
    * Update Turma and override ToString()

--- ProjetoIntegradorIIIA/Enums/EtapaEnsinoEnum.cs
+++ ProjetoIntegradorIIIA/Enums/EtapaEnsinoEnum.cs
@@ -1,10 +1,19 @@
-﻿namespace ProjetoIntegradorIIIA.Enums
+﻿using System.ComponentModel;
+
+namespace ProjetoIntegradorIIIA.Enums
 {
     public enum EtapaEnsinoEnum
     {
+        [Description("Educação Infantil")]
         Infantil = 0,
+
+        [Description("Ensino Fundamental Anos Iniciais")]
         FundamentalAnosIniciais = 1,
+
+        [Description("Ensino Fundamental Anos Finais")]
         FundamentalAnosFinais = 2,
+
+        [Description("Ensino Médio")]
         Medio = 3,
     }
 }
---
+++ ProjetoIntegradorIIIA/Models/Escola.cs
@@ -0,0 +1,58 @@
+﻿namespace ProjetoIntegradorIIIA.Models
+{
+    class Escola
+    {
+        public List<Turma> Turmas { get; private set; }
+        public ListaDeAluno.ListaDeAluno Alunos { get; private set; }
+
+        public Escola()
+        {
+            Turmas = new List<Turma>();
+            Alunos = new ListaDeAluno.ListaDeAluno();
+        }
+
+        public void CadastrarTurma(Turma turma)
+        {
+            if (turma == null)
+                throw new ArgumentNullException(nameof(turma), "Turma não pode ser nula.");
+
+            Turmas.Add(turma);
+        }
+
+        public void CadastrarAluno(Aluno aluno)
+        {
+            if (aluno == null)
+                throw new ArgumentNullException(nameof(aluno), "Aluno não pode ser nulo.");
+
+            Alunos.IncluirNoFim(aluno);
+        }
+
+        /// <summary>
+        /// Matricula um aluno em uma turma, validando que:
+        /// - A turma exista.
+        /// - O aluno exista (na lista de alunos da escola).
+        /// - O aluno não esteja já matriculado na turma.
+        /// - A turma ainda tenha vagas.
+        /// </summary>
+        /// <param name="codigoTurma">Código da turma</param>
+        /// <param name="codigoAluno">Código do aluno</param>
+        public void MatricularAluno(int codigoTurma, int codigoAluno)
+        {
+            Turma? turma = Turmas.FirstOrDefault(t => t.Codigo == codigoTurma);
+            if (turma == null)
+                throw new Exception("Turma não encontrada.");
+
+            Aluno? aluno = Alunos.GetByCodigo(codigoAluno);
+            if (aluno == null)
+                throw new Exception("Aluno não encontrado.");
+
+            if (turma.Alunos.GetByCodigo(codigoAluno) != null)
+                throw new Exception("Aluno já matriculado nesta turma.");
+
+            if (turma.QuantidadeMatriculados >= turma.LimiteVagas)
+                throw new Exception("A turma já atingiu o limite de vagas.");
+
+            turma.Alunos.IncluirNoFim(aluno);
+        }
+    }
+}
