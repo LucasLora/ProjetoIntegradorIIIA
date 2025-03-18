2c03495a267102c9b1982c25b65f7049def99a23
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2025-03-18 20:58:06 -0300

    Create MenuOptionEnum

---
+++ ProjetoIntegradorIIIA/Enums/MenuOptionEnum.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace ProjetoIntegradorIIIA.Enums
+{
+    public enum MenuOptionEnum
+    {
+        [Description("Sair")]
+        Sair = 0,
+
+        [Description("Cadastrar Aluno")]
+        CadastrarAluno = 1,
+
+        [Description("Cadastrar Turma")]
+        CadastrarTurma = 2,
+
+        [Description("Matricular Alunos em uma Turma")]
+        MatricularAlunosEmUmaTurma = 3,
+
+        [Description("Listar Todos os Alunos da Escola")]
+        ListarTodosOsAlunosDaEscola = 4,
+
+        [Description("Listar Todas as Turmas da Escola")]
+        ListarTodasAsTurmasDaEscola = 5,
+
+        [Description("Listar Alunos de uma Turma Específica")]
+        ListarAlunosDeUmaTurmaEspecifica = 6,
+
+        [Description("Contar Alunos Fora da Faixa Etária por Etapa de Ensino")]
+        ContarAlunosForaDaFaixaEtariaPorEtapaDeEnsino = 7
+    }
+}
